76e23c73c4f3d6dec61649d3037555899ab87432
devwithzain <ZainSoftwear11@gmail.com>
2024-10-01 00:47:04 +0500

    deleted

--- app/(root)/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "@/components/auth/login-form";
-
-export default function App() {
-	return (
-		<div className="w-full h-screen flex items-center justify-center">
-			<LoginForm />
-		</div>
-	);
-}
